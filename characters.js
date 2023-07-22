c839251f026861f565845c1230503108b460872e
aceade@github.com <philiprowlands90@gmail.com>
2023-07-22 12:28:11 +0100

    #3 fixing image links

--- characters.js
+++ characters.js
@@ -1,21 +1,21 @@
 const characters = [{
     title: "The Warlock",
-    url: "https://static.wikia.nocookie.net/xcom/images/9/99/WarlockHead.png/revision/latest?cb=20210708115709",
+    url: "./images/WarlockHead.webp",
     alt: "The Warlock",
     text: "One of the 'Chosen' - a recurring miniboss introduced in War of The Chosen. Easily the hammiest and most intense of the lot. And therefore the funniest to put in absurd situations."
 }, {
     title: "The Hunter",
-    url: "https://static.wikia.nocookie.net/xcom/images/2/2a/HunterHead.png/revision/latest?cb=20210708115707",
+    url: "./images/HunterHead.webp",
     alt: "The Hunter",
     text: "One of the 'Chosen' - a recurring miniboss introduced in War of The Chosen. This one is the least motivated of the three, and probably the most likely to appreciate the humour here."
 }, {
     title: "The Assassin",
-    url: "https://static.wikia.nocookie.net/xcom/images/0/07/AssassinHead.png/revision/latest?cb=20210708115705",
+    url: "./images/AssassinHead.webp",
     alt: "The Hunter",
     text: "One of the 'Chosen' - a recurring miniboss introduced in War of The Chosen. She's probably the scariest of the three."
 }, {
     title: "The Speaker",
-    url: "https://static.wikia.nocookie.net/xcom/images/1/1b/XCOM2_Speaker.png/revision/latest?cb=20220205112345",
+    url: "./images/XCOM2_Speaker.webp",
     alt: "The Speaker",
     text: "The aliens' lead propaganda mouthpiece. Also known as 'Lanky Evil Garrus' in certain mods." 
 }];
